09a977f8fb66967c107b7db067210b517c921964
Olaf Naruszko <olaf.naruszko@gmail.com>
2025-01-16 13:32:25 +0100

    Remove function startGame from game.c file

--- include/game.h
+++ include/game.h
@@ -1,8 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
 
-void startGame();
-
 void processUserInput(Board *board, char command, int row, int col, int *gameOver, int mineCount);
 
 int checkIfWin(Board* board);
